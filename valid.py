b8b6470e91dab1ad30dc73c7520a0edbc7646122
subashdhanush <85820233+subashdhanush@users.noreply.github.com>
2025-03-19 16:23:38 +0530

    Add files via upload

---
+++ valid.py
@@ -0,0 +1,10 @@
+import re
+
+def validate_email(email):
+    pattern = r'^[a-zA-Z0-9._%+-]+@[a-zA-Z0-9.-]+\.[a-zA-Z]{2,}$'
+    if re.match(pattern, email):
+        print("Valid Email")
+    else:
+        print("Invalid Email")
+
+validate_email("test@example.com")
